603f1c376cac8d77638e02dcd051e1d921934317
Minjung <mkang9215@gmail.com>
2021-07-29 16:28:11 -0400

    commit

---
+++ src/app/new-releases/new-releases.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+import * as data from '../data/NewReleasesAlbums.json';
+
+@Component({
+  selector: 'app-new-releases',
+  templateUrl: './new-releases.component.html',
+  styleUrls: ['./new-releases.component.css']
+})
+export class NewReleasesComponent implements OnInit {
+  releases: any;
+  
+  constructor() { }
+
+  ngOnInit(): void {
+    
+  this.releases = (data as any).default.albums.items;
+  }
+
+}
